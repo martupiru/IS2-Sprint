5851a53ee0d36dc82097af64a20d393323ffe08c
martupiru <martunahman@gmail.com>
2025-09-03 17:39:43 -0300

    Base datos Videojuegos

--- TP1/Ejercicio 4/code/Dump20250903.sql
+++ TP1/Ejercicio 4/code/Dump20250903.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `videojuegos` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `videojuegos`;
 -- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
 --
 -- Host: localhost    Database: videojuegos
@@ -92,7 +90,7 @@ CREATE TABLE `videojuegos` (
   CONSTRAINT `FKev9v8m1hnvqlpne73pgko027c` FOREIGN KEY (`fk_estudio`) REFERENCES `estudios` (`id`),
   CONSTRAINT `videojuegos_chk_1` CHECK (((`precio` <= 10000) and (`precio` >= 5))),
   CONSTRAINT `videojuegos_chk_2` CHECK (((`stock` >= 0) and (`stock` <= 10000)))
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +99,7 @@ CREATE TABLE `videojuegos` (
 
 LOCK TABLES `videojuegos` WRITE;
 /*!40000 ALTER TABLE `videojuegos` DISABLE KEYS */;
-INSERT INTO `videojuegos` VALUES (1,_binary '','Aventura y exploración con Lara Croft','2013-03-05 00:00:00.000000','sprintgames.png',1500,30,'Tomb Raider',1,1),(2,_binary '','RPG independiente con elecciones morales','2015-09-14 03:00:00.000000','sprintgames.png',500,25,'Undertale',2,2),(3,_binary '','Construcción y aventuras en mundo abierto','2011-11-17 03:00:00.000000','sprintgames.png',1200,0,'Minecraft',3,3),(4,_binary '','Aventura basada en la película Shrek 2 locuraaa','2004-05-18 03:00:00.000000','sprintgames.png',800,15,'Shrek2',4,4),(5,_binary '','Juego que le gusta al Emi :)','2024-03-09 03:00:00.000000','1756330338284.jpg',950,9,'Skyrim',1,3),(6,_binary '','Juego para reirte de la marti jajajaj','2025-07-10 03:00:00.000000','peak.jpeg',159,7,'Peak',1,4),(7,_binary '','Sale COC?','2015-11-13 03:00:00.000000','1756421094654.png',6,45,'Clash of Clans',1,1);
+INSERT INTO `videojuegos` VALUES (1,_binary '','Aventura y exploración con Lara Croft','2013-03-05 00:00:00.000000','sprintgames.png',1500,30,'Tomb Raider',1,1),(2,_binary '','RPG independiente con elecciones morales','2015-09-14 03:00:00.000000','sprintgames.png',500,25,'Undertale',2,2),(3,_binary '','Construcción y aventuras en mundo abierto','2011-11-17 03:00:00.000000','sprintgames.png',1200,0,'Minecraft',3,3),(4,_binary '\0','Aventura basada en la película Shrek 2 locuraaa','2004-05-18 03:00:00.000000','sprintgames.png',800,15,'Shrek2',4,4),(5,_binary '','Juego que le gusta al Emi :)','2024-03-09 03:00:00.000000','sky.jpg',950,9,'Skyrim',1,3),(6,_binary '','Juego para reirte de la marti jajajaj','2025-07-10 03:00:00.000000','peak.jpeg',159,7,'Peak',1,4),(7,_binary '','Sale COC?','2015-11-13 03:00:00.000000','1756421094654.png',6,45,'Clash of Clans',1,1),(8,_binary '','Inspirado en el Call of Duty ','2018-06-09 03:00:00.000000','1756928385194.jpg',877,24,'Call of Duty Mobile',1,1),(9,_binary '','fuboooool','2011-06-26 03:00:00.000000','1756928649993.jpg',241,50,'FIFA 22',1,2);
 /*!40000 ALTER TABLE `videojuegos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -114,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-08-30 17:59:13
+-- Dump completed on 2025-09-03 17:38:57
